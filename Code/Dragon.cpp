e7637277874335b006ba0b4612dc14fe8ab420ba
Nikita Oelofse <u19015951@tuks.co.za>
2021-11-19 14:30:40 +0200

    Added methods to cpp files of Dragon, DragonSpacecraft, CrewDragon, Contents, Humans, Cargo

--- Code/Dragon.cpp
+++ Code/Dragon.cpp
@@ -1,5 +1,26 @@
 //
 // Created by Conrad Strasheim on 2021/11/15.
 //
+// Edited by Nikita Oelofse on 2021/11/19
 
 #include "Dragon.h"
+
+Dragon::Dragon() {
+
+}
+
+Dragon::~Dragon() {
+
+}
+
+void Dragon::add(Dragon*) {
+
+}
+
+void Dragon::remove() { //NOT SURE IF NECESSARY
+
+}
+
+void Dragon::handleContent() {
+
+}
--- Code/DragonSpacecraft.cpp
+++ Code/DragonSpacecraft.cpp
@@ -1,5 +1,18 @@
 //
 // Created by Conrad Strasheim on 2021/11/15.
 //
+// Edited by Nikita Oelofse on 2021/11/19
 
 #include "DragonSpacecraft.h"
+
+DragonSpacecraft::DragonSpacecraft() {
+
+}
+
+DragonSpacecraft::~DragonSpacecraft() {
+
+}
+
+void DragonSpacecraft::handleContent() {
+    
+}
