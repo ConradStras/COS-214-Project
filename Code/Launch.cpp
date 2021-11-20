a0c2f0a696b7ff378cca5317a11d74c543229d15
arlo216 <charlottevzyl1@gmail.com>
2021-11-20 10:08:08 +0200

    cleaning up of FalconRocket

--- Code/Launch.cpp
+++ Code/Launch.cpp
@@ -4,19 +4,17 @@
 
 #include "Launch.h"
 //
-//void Launch::fireUpFalconHeavy() {
-//
-//    if( rocket->stages[0]->getEngineNo()== 27 && rocket->stages[1]->getEngineNo == 1)
-//        cout<<"The Falcon Heavy is now preparing to launch"<<endl;
-//    else
-//        cout<<"The Falcon Heavy cannot launch without its full engines."<<endl;
-//}
-//
-//void Launch::fireUpFalconNine() {
-//    if(rocket->stages[0]->getEngineNo() == 9 && rocket->stages[1]->getEngineNo()==1)
-//        cout<<"The Falcon 9 is ready to launch."<<endl;
-//    else
-//        cout<<"The Falcon 9 is not ready to launch."<<endl;
-//}
+void Launch::fireUpFalconHeavy() {
 
-//uncomment
+    if( rocket->stages[0]->getEngineNo()== 27 && rocket->stages[1]->getEngineNo == 1)
+        cout<<"The Falcon Heavy is now preparing to launch"<<endl;
+    else
+        cout<<"The Falcon Heavy cannot launch without its full engines."<<endl;
+}
+
+void Launch::fireUpFalconNine() {
+    if(rocket->stages[0]->getEngineNo() == 9 && rocket->stages[1]->getEngineNo()==1)
+        cout<<"The Falcon 9 is ready to launch."<<endl;
+    else
+        cout<<"The Falcon 9 is not ready to launch."<<endl;
+}
